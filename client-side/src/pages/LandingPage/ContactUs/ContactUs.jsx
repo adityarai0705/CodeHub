9fc39d308b32ee40dbb5604559ca9b6ebd8296bd
ArchanaY1203 <ay1840450@gmail.com>
2023-07-01 18:31:54 +0530

    ContactUs Added

--- client-side/src/pages/LandingPage/ContactUs/ContactUs.jsx
+++ client-side/src/pages/LandingPage/ContactUs/ContactUs.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './ContactUs.css';
 
 export default function ContactUs() {
+
+  //SUBMIT FUNCTION
+  const demoFunction = (e) => {
+    e.preventDefault();
+    console.log("Submit Buttton Working");
+  }
+
+
   return (
-    <div>
-      
-    </div>
+    <section id="contactpageid">
+      <div id='contactheadlinecontainer'>
+        <h1 className="contact12headline">ContactUs</h1>
+      </div>
+      <div id='contact-boxContactusid' >
+        <form>
+          <input type="text" placeholder='Enter your name' name='name' />
+          <input type="email" placeholder='Email' name='email' />
+          <textarea type="text" id='Contactmsg' placeholder='Enter your message' name='message' cols="30" rows="10" />
+          <button className='ContactUspagebtn' onClick={(e) => { demoFunction(e) }}>Submit</button>
+        </form>
+      </div>
+    </section>
   )
 }
