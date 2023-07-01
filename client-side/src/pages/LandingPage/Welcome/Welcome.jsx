f6fb4d755504d9f3ff557b03055a2eb540d7bf22
32bitdev <32bitdev@gmail.com>
2023-07-01 14:13:51 +0530

    Welcome Page Added

--- client-side/src/pages/LandingPage/Welcome/Welcome.jsx
+++ client-side/src/pages/LandingPage/Welcome/Welcome.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import "./Welcome.css"
+import CodeTogetherLarge from "../Assets/Logos/CodeTogetherLarge.png"
+
 
 export default function Welcome() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div id="welcomeMain">
+            <div id='welcomeBoxMain'>
+                <div id='welcomeMessageMain'>code?! Nahhh....<br />Tell me your approach<br />and we'll</div>
+                <div id='welcomePhotoGreetMain'>
+                    <img src={CodeTogetherLarge} alt="Code Together" />
+                </div>
+            </div>
+        </div>
+    )
 }
