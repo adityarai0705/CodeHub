ca5421de3ce7b817fdd8268d5282875e0bb903f4
ArchanaY1203 <ay1840450@gmail.com>
2023-07-01 17:26:20 +0530

    Teams Added

--- client-side/src/pages/LandingPage/Teams/Teams.jsx
+++ client-side/src/pages/LandingPage/Teams/Teams.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import "./Teams.css";
+import { useNavigate } from 'react-router-dom';
 
 export default function Teams() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const navigate = useNavigate();
+    
+    return (
+        <div id='teams1Main'>
+            <div id='teams2Main'>
+                <div id='teams3Main'>
+                    <div id='teamsTitleMain'>
+                        Teams
+                    </div>
+                    <div id='teamsFlexContainer'>
+                        <div id='teamsContentMain'>
+                            <div onClick={() => navigate("/demoLink")} className='teamContentBoxesMain'>
+                                <p>CT Admins</p>
+                            </div>
+                            <div onClick={() => navigate("/demoLink")} className='teamContentBoxesMain'>
+                                <p>CT Representatives</p>
+                            </div>
+                            <div onClick={() => navigate("/demoLink")} className='teamContentBoxesMain'>
+                                <p>CT Design Team</p>
+                            </div>
+                            <div onClick={() => navigate("/demoLink")} className='teamContentBoxesMain'>
+                                <p>CT Web Team</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
