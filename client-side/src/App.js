5fe1cecf40825124cac7f31236c6f82cc70742e7
32bitdev <32bitdev@gmail.com>
2023-07-01 23:24:57 +0530

    Frontend Login Route Added

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter >
   )
