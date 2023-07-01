accd301a5bdb2acab629c4f6e5f1da3537c4dce0
32bitdev <32bitdev@gmail.com>
2023-07-01 19:09:18 +0530

    Frontend Routes & Assets Added

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter >
   )
 }
