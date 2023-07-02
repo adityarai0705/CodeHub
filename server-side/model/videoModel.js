b897a44ca98c28373c6c4d7086373c1e2f76fda0
32bitdev <32bitdev@gmail.com>
2023-07-02 23:05:07 +0530

    Mongoose Model Updated

---
+++ server-side/model/videoModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const videoSchema = new mongoose.Schema({
+    categoryID: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 100,
+        unique: true,
+    },
+    title: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 100,
+        unique: true,
+    },
+    ytLink: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 100,
+        unique: true,
+    },
+    visible: {
+        type: Boolean,
+        default: true,
+    }
+});
+
+module.exports = mongoose.model("Videos", videoSchema);
