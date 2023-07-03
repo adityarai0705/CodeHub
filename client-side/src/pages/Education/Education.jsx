51294700bdcc2e387b25d9eb5b02355464d8ef62
kagarwal1811 <kushag203221@gmail.com>
2023-07-03 20:18:53 +0530

    created education section

---
+++ client-side/src/pages/Education/Education.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import '../pages.css'
+import './Education.css'
+
+// educationCategories is a dummy data
+const educationCategories = [
+    {
+        title : 'Editorials',
+        _id : 'dummy232234'
+    },
+    {
+        title : 'Dynamic Programming',
+        _id : 'dummy232234'
+    },
+    {
+        title : 'Greedy',
+        _id : 'dummy232234'
+    },
+    {
+        title : 'Graphs',
+        _id : 'dummy232234'
+    },
+    {
+        title : 'Backtracking',
+        _id : 'dummy232234'
+    },
+    {
+        title : 'C/C++',
+        _id : 'dummy232234'
+    },
+    {
+        title : 'Java',
+        _id : 'dummy232234'
+    },
+    {
+        title : 'Game Theory',
+        _id : 'dummy232234'
+    }
+];
+
+function EduSection(props)
+{
+    return (
+        <div>
+            <div className="edu-box">
+                <div className="edu-title">{props.title}</div>
+            </div>
+        </div>
+    );
+}
+
+function CreateList(props)
+{
+    return (
+        <EduSection
+         key={props._id}
+         title={props.title}
+         />
+    );
+}
+
+export default function Education() {
+  return (
+    <div className="background-pink-blue">
+        <div className="education-heading">Education Section</div>
+        {educationCategories.map(EduSection)}
+    </div>
+  )
+}
