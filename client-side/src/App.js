ee61f92906ea95b902df0e355720773263273717
adityarai0705 <aditya70544@icloud.com>
2023-07-05 00:51:58 +0530

    merged Noticeboard

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -4,6 +4,7 @@ import LandingPage from "./pages/LandingPage/LandingPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import VideoLists from './pages/VideoLists/VideoLists';
 import Education from './pages/Education/Education';
+import NoticeBoard from './pages/NoticeBoard/NoticeBoard';
 
 export default function App() {
   return (
@@ -12,7 +13,8 @@ export default function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/education/videos" element={<VideoLists />} />
-        <Route path="/education" element={<Education/>} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/notice" element={<NoticeBoard />} />
       </Routes>
     </BrowserRouter >
   )
