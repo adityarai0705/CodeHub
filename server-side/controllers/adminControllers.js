d2f9ccaf27d7cf456e78d382080d92269417ac1c
ArchanaY1203 <ay1840450@gmail.com>
2023-07-05 19:31:58 +0530

    Admit Controllers Updated

--- server-side/controllers/adminControllers.js
+++ server-side/controllers/adminControllers.js
@@ -2,6 +2,8 @@ const EducationCategories = require("../model/educationCategoryModel");
 const Feedback = require("../model/feedbackModel");
 const Notices = require("../model/noticeModel");
 const Videos = require("../model/videoModel");
+const Admins = require("../model/adminModel");
+const AdminSessions = require("../model/adminSessionModel");
 
 module.exports.adminLogin = async (req, res, next) => {
     try {
@@ -31,8 +33,24 @@ module.exports.adminLogin = async (req, res, next) => {
 
 module.exports.notice = async (req, res, next) => {
     try {
-        const notices = await Notices.find();
-        return res.json({ status: true, data: notices });
+        let cookieID;
+        const cookie = req.cookies.jwt;
+        jwt.verify(
+            cookie,
+            process.env.COOKIE_SECRET_KEY,
+            (err, decoded) => {
+                if (err)
+                    return res.json({ status: false, msg: "Invalid cookieID" });
+                cookieID = decoded.cookieID;
+            }
+        );
+        const session = await AdminSessions.findOne({ username: username });
+        if (cookieID == session.cookieID) {
+            const notices = await Notices.find();
+            return res.json({ status: true, data: notices });
+        }
+        else
+            return res.json({ status: false, msg: "Session expired" });
     }
     catch (ex) {
         next(ex);
@@ -42,11 +60,27 @@ module.exports.notice = async (req, res, next) => {
 
 module.exports.noticeDelete = async (req, res, next) => {
     try {
-        const { noticeID } = req.body;
-        const notice = await Notices.find({ _id: noticeID });
-        if (!notice) return res.json({ status: false, msg: "Notice doesn't exist" });
-        await Notices.remove({ _id: noticeID });
-        return res.json({ status: true });
+        let cookieID;
+        const cookie = req.cookies.jwt;
+        jwt.verify(
+            cookie,
+            process.env.COOKIE_SECRET_KEY,
+            (err, decoded) => {
+                if (err)
+                    return res.json({ status: false, msg: "Invalid cookieID" });
+                cookieID = decoded.cookieID;
+            }
+        );
+        const session = await AdminSessions.findOne({ username: username });
+        if (cookieID == session.cookieID) {
+            const { noticeID } = req.body;
+            const notice = await Notices.find({ _id: noticeID });
+            if (!notice) return res.json({ status: false, msg: "Notice doesn't exist" });
+            await Notices.remove({ _id: noticeID });
+            return res.json({ status: true });
+        }
+        else
+            return res.json({ status: false, msg: "Session expired" });
     }
     catch (ex) {
         next(ex);
@@ -56,11 +90,27 @@ module.exports.noticeDelete = async (req, res, next) => {
 
 module.exports.noticeCreate = async (req, res, next) => {
     try {
-        const { title, body } = req.body;
-        const notice = await Notices.find({ title: title });
-        if (notice) return res.json({ status: false, msg: "Notice title already exist" });
-        await Notices.create({ title: title, body: body });
-        return res.json({ status: true });
+        let cookieID;
+        const cookie = req.cookies.jwt;
+        jwt.verify(
+            cookie,
+            process.env.COOKIE_SECRET_KEY,
+            (err, decoded) => {
+                if (err)
+                    return res.json({ status: false, msg: "Invalid cookieID" });
+                cookieID = decoded.cookieID;
+            }
+        );
+        const session = await AdminSessions.findOne({ username: username });
+        if (cookieID == session.cookieID) {
+            const { title, body } = req.body;
+            const notice = await Notices.find({ title: title });
+            if (notice) return res.json({ status: false, msg: "Notice title already exist" });
+            await Notices.create({ title: title, body: body });
+            return res.json({ status: true });
+        }
+        else
+            return res.json({ status: false, msg: "Session expired" });
     }
     catch (ex) {
         next(ex);
@@ -70,8 +120,24 @@ module.exports.noticeCreate = async (req, res, next) => {
 
 module.exports.educationCategories = async (req, res, next) => {
     try {
-        const educationCategories = await EducationCategories.find();
-        return res.json({ status: true, data: educationCategories });
+        let cookieID;
+        const cookie = req.cookies.jwt;
+        jwt.verify(
+            cookie,
+            process.env.COOKIE_SECRET_KEY,
+            (err, decoded) => {
+                if (err)
+                    return res.json({ status: false, msg: "Invalid cookieID" });
+                cookieID = decoded.cookieID;
+            }
+        );
+        const session = await AdminSessions.findOne({ username: username });
+        if (cookieID == session.cookieID) {
+            const educationCategories = await EducationCategories.find();
+            return res.json({ status: true, data: educationCategories });
+        }
+        else
+            return res.json({ status: false, msg: "Session expired" });
     }
     catch (ex) {
         next(ex);
@@ -81,11 +147,27 @@ module.exports.educationCategories = async (req, res, next) => {
 
 module.exports.educationDelete = async (req, res, next) => {
     try {
-        const { educationID } = req.body;
-        const educationCategory = await EducationCategories.find({ _id: educationID });
-        if (!educationCategory) return res.json({ status: false, msg: "Education Category doesn't exist" });
-        await EducationCategories.remove({ _id: educationCategory });
-        return res.json({ status: true });
+        let cookieID;
+        const cookie = req.cookies.jwt;
+        jwt.verify(
+            cookie,
+            process.env.COOKIE_SECRET_KEY,
+            (err, decoded) => {
+                if (err)
+                    return res.json({ status: false, msg: "Invalid cookieID" });
+                cookieID = decoded.cookieID;
+            }
+        );
+        const session = await AdminSessions.findOne({ username: username });
+        if (cookieID == session.cookieID) {
+            const { educationID } = req.body;
+            const educationCategory = await EducationCategories.find({ _id: educationID });
+            if (!educationCategory) return res.json({ status: false, msg: "Education Category doesn't exist" });
+            await EducationCategories.remove({ _id: educationCategory });
+            return res.json({ status: true });
+        }
+        else
+            return res.json({ status: false, msg: "Session expired" });
     }
     catch (ex) {
         next(ex);
@@ -95,11 +177,27 @@ module.exports.educationDelete = async (req, res, next) => {
 
 module.exports.educationCreate = async (req, res, next) => {
     try {
-        const { title } = req.body;
-        const educationCategory = await EducationCategories.find({ title: title });
-        if (educationCategory) return res.json({ status: false, msg: "Education Category title already exist" });
-        await EducationCategories.create({ title: title });
-        return res.json({ status: true });
+        let cookieID;
+        const cookie = req.cookies.jwt;
+        jwt.verify(
+            cookie,
+            process.env.COOKIE_SECRET_KEY,
+            (err, decoded) => {
+                if (err)
+                    return res.json({ status: false, msg: "Invalid cookieID" });
+                cookieID = decoded.cookieID;
+            }
+        );
+        const session = await AdminSessions.findOne({ username: username });
+        if (cookieID == session.cookieID) {
+            const { title } = req.body;
+            const educationCategory = await EducationCategories.find({ title: title });
+            if (educationCategory) return res.json({ status: false, msg: "Education Category title already exist" });
+            await EducationCategories.create({ title: title });
+            return res.json({ status: true });
+        }
+        else
+            return res.json({ status: false, msg: "Session expired" });
     }
     catch (ex) {
         next(ex);
@@ -109,8 +207,24 @@ module.exports.educationCreate = async (req, res, next) => {
 
 module.exports.videos = async (req, res, next) => {
     try {
-        const videos = await Videos.find();
-        return res.json({ status: true, data: videos });
+        let cookieID;
+        const cookie = req.cookies.jwt;
+        jwt.verify(
+            cookie,
+            process.env.COOKIE_SECRET_KEY,
+            (err, decoded) => {
+                if (err)
+                    return res.json({ status: false, msg: "Invalid cookieID" });
+                cookieID = decoded.cookieID;
+            }
+        );
+        const session = await AdminSessions.findOne({ username: username });
+        if (cookieID == session.cookieID) {
+            const videos = await Videos.find();
+            return res.json({ status: true, data: videos });
+        }
+        else
+            return res.json({ status: false, msg: "Session expired" });
     }
     catch (ex) {
         next(ex);
@@ -120,11 +234,27 @@ module.exports.videos = async (req, res, next) => {
 
 module.exports.videoDelete = async (req, res, next) => {
     try {
-        const { videoID } = req.body;
-        const video = await Videos.find({ _id: videoID });
-        if (!video) return res.json({ status: false, msg: "Video doesn't exist" });
-        await Videos.remove({ _id: videoID });
-        return res.json({ status: true });
+        let cookieID;
+        const cookie = req.cookies.jwt;
+        jwt.verify(
+            cookie,
+            process.env.COOKIE_SECRET_KEY,
+            (err, decoded) => {
+                if (err)
+                    return res.json({ status: false, msg: "Invalid cookieID" });
+                cookieID = decoded.cookieID;
+            }
+        );
+        const session = await AdminSessions.findOne({ username: username });
+        if (cookieID == session.cookieID) {
+            const { videoID } = req.body;
+            const video = await Videos.find({ _id: videoID });
+            if (!video) return res.json({ status: false, msg: "Video doesn't exist" });
+            await Videos.remove({ _id: videoID });
+            return res.json({ status: true });
+        }
+        else
+            return res.json({ status: false, msg: "Session expired" });
     }
     catch (ex) {
         next(ex);
@@ -134,11 +264,27 @@ module.exports.videoDelete = async (req, res, next) => {
 
 module.exports.videoCreate = async (req, res, next) => {
     try {
-        const { title, ytLink } = req.body;
-        const video = await Videos.find({ $or: [{ title: title }, { ytLink: ytLink }] });
-        if (video) return res.json({ status: false, msg: "Video already exist" });
-        await Videos.create({ title: title });
-        return res.json({ status: true });
+        let cookieID;
+        const cookie = req.cookies.jwt;
+        jwt.verify(
+            cookie,
+            process.env.COOKIE_SECRET_KEY,
+            (err, decoded) => {
+                if (err)
+                    return res.json({ status: false, msg: "Invalid cookieID" });
+                cookieID = decoded.cookieID;
+            }
+        );
+        const session = await AdminSessions.findOne({ username: username });
+        if (cookieID == session.cookieID) {
+            const { title, ytLink } = req.body;
+            const video = await Videos.find({ $or: [{ title: title }, { ytLink: ytLink }] });
+            if (video) return res.json({ status: false, msg: "Video already exist" });
+            await Videos.create({ title: title });
+            return res.json({ status: true });
+        }
+        else
+            return res.json({ status: false, msg: "Session expired" });
     }
     catch (ex) {
         next(ex);
@@ -148,8 +294,24 @@ module.exports.videoCreate = async (req, res, next) => {
 
 module.exports.feedback = async (req, res, next) => {
     try {
-        const feedback = await Feedback.find();
-        return res.json({ status: true, data: feedback });
+        let cookieID;
+        const cookie = req.cookies.jwt;
+        jwt.verify(
+            cookie,
+            process.env.COOKIE_SECRET_KEY,
+            (err, decoded) => {
+                if (err)
+                    return res.json({ status: false, msg: "Invalid cookieID" });
+                cookieID = decoded.cookieID;
+            }
+        );
+        const session = await AdminSessions.findOne({ username: username });
+        if (cookieID == session.cookieID) {
+            const feedback = await Feedback.find();
+            return res.json({ status: true, data: feedback });
+        }
+        else
+            return res.json({ status: false, msg: "Session expired" });
     }
     catch (ex) {
         next(ex);
