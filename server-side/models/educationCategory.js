14dd51bcb61dc17fd236a2a6c5bb436ebe76e159
adityarai0705 <aditya70544@icloud.com>
2023-10-04 17:05:38 +0530

    removed temporary server

--- server-side/models/educationCategory.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const educationSchema = new Schema({
-    title : String,
-    visible : Boolean,
-});
-
-module.exports = mongoose.model( "education", educationSchema);
--- server-side/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema({
-    cfID : String,
-    visible : Boolean,
-});
-
-module.exports = mongoose.model( "user", userSchema);
